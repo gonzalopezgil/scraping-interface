bab97b23837d9196f304a13d7c43c1b8272d6708
Gonzalo López Gil <gonzalo.lopezg@edu.uah.es>
2023-03-20 23:36:47 +0100

    Update> Copy to clipboard the clicked text

--- gui/JavaScriptStrings.py
+++ gui/JavaScriptStrings.py
@@ -5,6 +5,7 @@ ENABLE_LINKS_JS = """
     }
 """
 
+# The last part is from HIGHLIGHT_TEXT_JS
 DISABLE_LINKS_JS = """
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
@@ -14,7 +15,33 @@ DISABLE_LINKS_JS = """
     function disableLink(event) {
         event.preventDefault();
     }
+
+    var textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, li, a, td, th');
+    for (var i = 0; i < textElements.length; i++) {
+        textElements[i].addEventListener("click", copyToClipboard);
+    }
+
+    function copyToClipboard(event) {
+        var selectedText = event.target.innerText.trim();
+        if (selectedText) {
+            var textarea = document.createElement("textarea");
+            document.body.appendChild(textarea);
+            textarea.value = selectedText;
+            textarea.select();
+            document.execCommand("copy");
+            document.body.removeChild(textarea);
+
+            var style = document.createElement('style');
+            document.head.appendChild(style);
+            var range = document.createRange();
+            range.selectNodeContents(document.body);
+            var selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
 """
+
 
 HIGHLIGHT_TEXT_JS = """
     var style = document.createElement('style');
