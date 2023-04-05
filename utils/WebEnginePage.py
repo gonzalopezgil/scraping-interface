89b05b3cdf028d02de53d208ce18db0ef48e6b58
Gonzalo López Gil <gonzalo.lopezg@edu.uah.es>
2023-04-05 21:37:10 +0200

    fix: bug with duplicated xpaths

--- utils/WebEnginePage.py
+++ utils/WebEnginePage.py
@@ -1,21 +1,25 @@
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWidgets import QTableWidgetItem
+import pyperclip
 
 class WebEnginePage(QWebEnginePage):
     table_widget = None
     last_column = -1
-    clicked_text = None
+    clicked_xpath = None
+    clicked_text = ""
 
     def reset_table(self):
         self.last_column = -1
-        self.clicked_text = None
+        self.clicked_xpath = None
 
     def javaScriptConsoleMessage(self, level, message, lineNumber, sourceID):
         print(f"JavaScript Message: {message}")
-        if message.startswith("/html") and message != self.clicked_text:
+        text = pyperclip.paste()
+        if message.startswith("/html") and message != self.clicked_xpath and text != self.clicked_text:
             if self.table_widget.rowCount() == 1:
                 self.table_widget.insertRow(1)
             self.last_column += 1
             self.table_widget.setItem(1, self.last_column, QTableWidgetItem(message))
-            self.clicked_text = message
+            self.clicked_xpath = message
+            self.clicked_text = text
         
