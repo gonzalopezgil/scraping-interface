9ba970bc44f2ef9c89b01106c25f6cfb29f97ff2
Gonzalo López Gil <74659017+gonzalopezgil@users.noreply.github.com>
2023-05-11 15:09:34 +0200

    chore: requirements.txt for windows updated

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from gui import main
-import os
 
 if __name__ == '__main__':
-    # Only for testing purposes
-    os.system('rm -rf *.xlsx *.csv *.json *.xml')
-
     # Run the application
     main()
