1d97a8d741c375e4c8d4a7eac66c8cdda34978ef
Gonzalo López Gil <74659017+gonzalopezgil@users.noreply.github.com>
2023-05-11 15:09:50 +0200

    feat: icon for the desktop app

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets import QApplication
 from gui.main_window import MainWindow
+from PyQt5.QtGui import QIcon
 from utils.password_manager import create_key
 import sys
 import io
+from static import icon_path
 
 def main():
 
@@ -17,6 +19,7 @@ def main():
 
     # Run the application
     app = QApplication([])
+    app.setWindowIcon(QIcon(icon_path))
     window = MainWindow()
     window.setMinimumSize(650, 850)
     window.show()
