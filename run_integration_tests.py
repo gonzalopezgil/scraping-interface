8cc5557d9fbbcc16d7d9a8a71e88f788aaa4d413
Gonzalo López Gil <gonzalo.lopezg@edu.uah.es>
2023-06-04 00:05:32 +0200

    feat: more information about integration tests

--- run_integration_tests.py
+++ run_integration_tests.py
@@ -6,6 +6,7 @@ import sys
 from scrapers.selenium_scraper import SeleniumScraper
 import csv
 from parameterized import parameterized
+import time
 
 # TEST CASES
 
@@ -23,8 +24,9 @@ selected_text = ['Mark', 'Otto', '@mdo']
 pagination_xpath = None
 max_pages = None
 expected_count = 6
+test_type = 'Static page'
 
-TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count))
+TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type))
 
 # 2. DYNAMIC PAGES (AJAX)
 url = 'https://webscraper.io/test-sites/e-commerce/ajax/computers/tablets'
@@ -37,8 +39,9 @@ selected_text = ['Lenovo IdeaTab', '12 reviews']
 pagination_xpath = 'fake'
 max_pages = 1
 expected_count = 6
+test_type = 'Dynamic page (AJAX)'
 
-TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count))
+TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type))
 
 # 3. PAGINATION
 url = 'https://webscraper.io/test-sites/e-commerce/static/computers/laptops'
@@ -51,8 +54,9 @@ selected_text = ['Packard 255 G2', '15.6", AMD E2-3800 1.3GHz, 4GB, 500GB, Windo
 pagination_xpath = '/html[1]/body[1]/div[1]/div[3]/div[1]/div[2]/nav[1]/ul[1]/li[15]/a[1]'
 max_pages = 20
 expected_count = 6 * 19 + 3
+test_type = 'Pagination'
 
-TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count))
+TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type))
 
 # 4. SCROLLING
 url = 'https://webscraper.io/test-sites/e-commerce/scroll/phones/touch'
@@ -65,8 +69,9 @@ selected_text = ['Nokia 123', '$24.99']
 pagination_xpath = 'fake'
 max_pages = 1
 expected_count = 9
+test_type = 'Scrolling'
 
-TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count))
+TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type))
 
 # 5. LOGIN
 url = 'https://the-internet.herokuapp.com/secure'
@@ -78,8 +83,9 @@ selected_text = ['Welcome to the Secure Area. When you are done click logout bel
 pagination_xpath = 'fake'
 max_pages = 1
 expected_count = 1
+test_type = 'Login'
 
-TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count))
+TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type))
 
 # 6. CAPTCHA
 url = 'https://www.google.com/recaptcha/api2/demo'
@@ -91,8 +97,9 @@ selected_text = ['Se ha verificado correctamente… ¡Genial!']
 pagination_xpath = 'fake'
 max_pages = 1
 expected_count = 1
+test_type = 'CAPTCHA'
 
-TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count))
+TEST_PARAMETERS.append((url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type))
 
 class IntegrationTest(unittest.TestCase):
 
@@ -108,8 +115,28 @@ class IntegrationTest(unittest.TestCase):
         # Initialize a MainWindow object
         self.main_window = MainWindow(self.app)
 
+    def print_test_parameters(self, url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type):
+        print(f"\nTEST: {test_type}")
+        print("----------------------------------------------------------------------")
+        print(f"url: {url}")
+        print(f"column_titles: {column_titles}")
+        print(f"xpaths: {xpaths}")
+        print(f"selected_text: {selected_text}")
+        print(f"pagination_xpath: {pagination_xpath}")
+        print(f"max_pages: {max_pages}")
+        print(f"expected_count: {expected_count}")
+        print("")
+
+    def calculate_elapsed_time(self, start_time):
+        elapsed_time = time.time() - start_time
+        minutes = int(elapsed_time / 60)
+        seconds = int(elapsed_time % 60)
+        return f"Elapsed Time: {minutes} minutes {seconds} seconds"
+
     @parameterized.expand(TEST_PARAMETERS)
-    def test_thread_function(self, url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count):
+    def test_thread_function(self, url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type):
+        self.print_test_parameters(url, column_titles, xpaths, selected_text, pagination_xpath, max_pages, expected_count, test_type)
+
         # Test parameters
         file_name = 'test_file.csv'
         row = 0
@@ -127,11 +154,18 @@ class IntegrationTest(unittest.TestCase):
             html = driver.page_source
             driver.quit()
 
+        # Start the timer
+        start_time = time.time()
+
         # Start the thread
         self.main_window.thread_function(url, column_titles, file_name, row, process_manager, signal_manager, None, html, max_pages=max_pages)
 
+        # Calculate elapsed time
+        elapsed_time_str = self.calculate_elapsed_time(start_time)
+        print(elapsed_time_str)
+
         # Check the file is saved correctly
-        self.assertTrue(os.path.exists(file_name))
+        self.assertTrue(os.path.exists(file_name), msg="The file was not saved correctly.")
 
         # Open the CSV file and check its contents
         row_count = 0
@@ -140,18 +174,20 @@ class IntegrationTest(unittest.TestCase):
 
             # Check column headers
             headers = next(reader)
-            self.assertEqual(headers, column_titles)
+            self.assertEqual(headers, column_titles, msg="Column headers do not match.")
 
             # Check the format of the data
             for row in reader:
-                self.assertEqual(len(row), len(column_titles))
+                self.assertEqual(len(row), len(column_titles), msg="Data format is incorrect.")
                 row_count += 1
         
         # Check the number of rows
-        self.assertEqual(row_count, expected_count)
+        self.assertEqual(row_count, expected_count, msg="Row count is incorrect.")
 
         # Remove the test file after the test
         os.remove(file_name)
+
+        print("----------------------------------------------------------------------")
 
 if __name__ == '__main__':
     unittest.main()
